f3aee8ca7813949a8b1b245d3d56a799e89e3e3b
Sergey Savtyra <pandinator@protonmail.com>
2018-12-16 02:40:56 +0300

    Header looks as it needs to

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -27,25 +27,24 @@ export default class Layout extends Component {
 							<h3 className="desktop-layout__content__header__info__title">Кирилл Велимиров</h3>
 							<img src={msgNew} className="desktop-layout__content__header__info__icon" alt="icon" />
 							<img src={notifNew} className="desktop-layout__content__header__info__icon" alt="icon" />
-							<div className="desktop-layout__content__header__info__page-name">
-								{this.props.pageName}
-							</div>
 						</div>
-						<Dropdown text="Добавить" icon="add" direction="left" floating labeled button className="icon desktop-layout__content__header__action">
-							<Dropdown.Menu>
-								<Dropdown.Header icon="hand point down" content="Выберите, что добавить" />
-								<Dropdown.Divider />
-								<Dropdown.Item onClick={e => this.navTo('/management/create-unit')} icon="home" text="Объект" />
-								<Dropdown.Item onClick={e => this.navTo('/management/add-room')} icon="building" text="Помещение" />
-								<Dropdown.Item onClick={e => this.navTo('/management/add-lead')} icon="user" text="Лид" />
-							</Dropdown.Menu>
-						</Dropdown>
-						<div className="desktop-layout__content__header__info__page-name desktop-layout__content__header__info__page-name--row">
-							<div className="row">
-								<img src={settings} className="desktop-layout__content__header__info__icon" alt="settings" />
-								<img src={logout} className="desktop-layout__content__header__info__icon" alt="logout" />
+						<div style={{ display: 'flex', alignItems: 'center' }}>
+							<Dropdown text="Добавить" icon="add" direction="left" floating labeled button className="icon desktop-layout__content__header__action">
+								<Dropdown.Menu>
+									<Dropdown.Header icon="hand point down" content="Выберите, что добавить" />
+									<Dropdown.Divider />
+									<Dropdown.Item onClick={e => this.navTo('/management/create-unit')} icon="home" text="Объект" />
+									<Dropdown.Item onClick={e => this.navTo('/management/add-room')} icon="building" text="Помещение" />
+									<Dropdown.Item onClick={e => this.navTo('/management/add-lead')} icon="user" text="Лид" />
+								</Dropdown.Menu>
+							</Dropdown>
+							<div className="desktop-layout__content__header__info__page-name desktop-layout__content__header__info__page-name--row">
+								<div className="row">
+									<img src={settings} className="desktop-layout__content__header__info__icon" alt="settings" />
+									<img src={logout} className="desktop-layout__content__header__info__icon" alt="logout" />
+								</div>
 							</div>
-						</div>
+						</div >
 					</header>
 					<Breadcrumbs
 						names={this.props.names}
